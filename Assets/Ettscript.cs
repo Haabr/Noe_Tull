2b739db851835b91aa1d42395f05ad48e3f2edda
Haabr <94066607+Haabr@users.noreply.github.com>
2022-03-11 10:55:35 +0100

    Update Ettscript.cs

--- Assets/Ettscript.cs
+++ Assets/Ettscript.cs
@@ -7,7 +7,7 @@ public class Ettscript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //change some behaviour
     }
 
     // Update is called once per frame
